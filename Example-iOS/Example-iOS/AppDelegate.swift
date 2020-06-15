f8ab9439f5c981086dde4a662ffd470c88434b70
Tomasz Szulc <mail@szulctomasz.com>
2020-06-15 18:41:35 +0200

    Create example project

---
+++ Example-iOS/Example-iOS/AppDelegate.swift
@@ -0,0 +1,14 @@
+// Example-iOS
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        true
+    }
+}
+
---
+++ Example-iOS/Example-iOS/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Tomasz Szulc on 15/06/2020.
+//  Copyright © 2020 Contentful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
