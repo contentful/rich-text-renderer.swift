8ef074e50c53af397eb0ec919a21acb16f42b529
Jingyu Kim <jingyu91@naver.com>
2022-12-01 17:43:44 +0900

    Add backgroundColor to RendererConfiguration

--- Sources/RichTextRenderer/Renderer/Default/DefaultRendererConfiguration.swift
+++ Sources/RichTextRenderer/Renderer/Default/DefaultRendererConfiguration.swift
@@ -4,6 +4,7 @@ import UIKit
 
 /// Default configuration for the `RichTextRenderer`.
 public struct DefaultRendererConfiguration: RendererConfiguration {
+    public var backgroundColor: UIColor = UIColor.rtrSystemBackground
     public var fontProvider: FontProviding = DefaultFontProvider()
     public var contentInsets: UIEdgeInsets = .init(top: 10, left: 10, bottom: 10, right: 15)
     public var textConfiguration: TextConfiguration = .default
